69b9ee2bda16be5d8425f377155c94659dcd7b43
rayhandz16 <rayhandz16@gmail.com>
2020-02-13 10:39:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, repudiandae! Illum eum sed doloremque sapiente, minus ab provident vero impedit velit rem! Ipsa, architecto voluptatibus animi iusto laboriosam possimus nihil.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
